6b516da118391220501fbdf9f5ef1ba03088c3b1
Erik Lindstrom <elindstrom42@gmail.com>
2020-11-16 13:45:40 -0600

    starting folder

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number");
+            string userResponse = Console.ReadLine();
+            int guess = int.Parse(userResponse);
+            Console.WriteLine(guess);
+        }
+    }
+}
