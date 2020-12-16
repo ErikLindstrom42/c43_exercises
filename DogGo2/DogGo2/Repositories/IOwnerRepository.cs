775535c29fe9e8dbfa30c54331416fb1010a735f
Erik Lindstrom <elindstrom42@gmail.com>
2020-12-16 11:57:47 -0600

    More Doggo

---
+++ DogGo2/DogGo2/Repositories/IOwnerRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+using DogGo2.Models;
+namespace DogGo2.Repositories
+{
+    public interface IOwnerRepository
+    {
+        SqlConnection Connection { get; }
+
+        void AddOwner(Owner owner);
+        void DeleteOwner(int ownerId);
+        Owner GetOwnerByEmail(string email);
+        Owner GetOwnerById(int id);
+        void UpdateOwner(Owner owner);
+    }
+}
